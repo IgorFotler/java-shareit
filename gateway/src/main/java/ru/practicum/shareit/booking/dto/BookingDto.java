aa61c8ffa5d26f5c5d5b5299fc23b250d7df78e3
Igor <IFotler@yandex.ru>
2025-07-15 00:58:33 +0700

    fix checkstyle

--- gateway/src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.booking.dto;
 
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.FutureOrPresent;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
